69fa36b7b312d73282030c73e1e8df5508912dbb
John Leider <john.j.leider@gmail.com>
2022-03-04 09:27:29 -0600

    fix(VSystemBar): add layout functionality

--- packages/vuetify/src/components/VSystemBar/VSystemBar.tsx
+++ packages/vuetify/src/components/VSystemBar/VSystemBar.tsx
@@ -5,13 +5,17 @@ import './VSystemBar.sass'
 import { makeBorderProps, useBorder } from '@/composables/border'
 import { makeDimensionProps, useDimension } from '@/composables/dimensions'
 import { makeElevationProps, useElevation } from '@/composables/elevation'
-import { makePositionProps, usePosition } from '@/composables/position'
 import { makeRoundedProps, useRounded } from '@/composables/rounded'
 import { makeTagProps } from '@/composables/tag'
 import { makeThemeProps, provideTheme } from '@/composables/theme'
+import { makeLayoutItemProps, useLayoutItem } from '@/composables/layout'
 
 // Utilities
 import { defineComponent } from '@/util'
+import { computed, ref, toRef } from 'vue'
+
+// Types
+import type { PropType } from 'vue'
 
 export const VSystemBar = defineComponent({
   name: 'VSystemBar',
@@ -19,26 +23,42 @@ export const VSystemBar = defineComponent({
   props: {
     lightsOut: Boolean,
     window: Boolean,
+    position: {
+      type: String as PropType<'top' | 'bottom'>,
+      default: 'top',
+      validator: (value: any) => ['top', 'bottom'].includes(value),
+    },
 
     ...makeBorderProps(),
-    ...makeDimensionProps(),
+    ...makeDimensionProps({ height: 32 }),
     ...makeElevationProps(),
-    ...makePositionProps(),
     ...makeRoundedProps(),
     ...makeTagProps(),
     ...makeThemeProps(),
+    ...makeLayoutItemProps(),
   },
 
   setup (props, { slots }) {
+    const vSystemBarRef = ref()
+    const height = computed(() => vSystemBarRef.value?.contentHeight)
+    const { layoutItemStyles } = useLayoutItem({
+      id: props.name,
+      priority: computed(() => parseInt(props.priority, 10)),
+      position: toRef(props, 'position'),
+      layoutSize: height,
+      elementSize: height,
+      active: ref(true),
+      absolute: toRef(props, 'absolute'),
+    })
     const { themeClasses } = provideTheme(props)
     const { borderClasses } = useBorder(props)
     const { dimensionStyles } = useDimension(props)
     const { elevationClasses } = useElevation(props)
-    const { positionClasses, positionStyles } = usePosition(props)
     const { roundedClasses } = useRounded(props)
 
     return () => (
       <props.tag
+        ref={ vSystemBarRef }
         class={[
           {
             'v-system-bar': true,
@@ -48,12 +68,11 @@ export const VSystemBar = defineComponent({
           themeClasses.value,
           borderClasses.value,
           elevationClasses.value,
-          positionClasses.value,
           roundedClasses.value,
         ]}
         style={[
           dimensionStyles.value,
-          positionStyles.value,
+          layoutItemStyles.value,
         ]}
         v-slots={ slots }
       />
